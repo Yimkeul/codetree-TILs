b5af4e95e2c0a512ae80c4f78c1cfdd0d2d8ee73
Yimkeul <leesjun29@gmail.com>
2024-01-23 22:05:35 +0900

    Title: 십진수와 이진수, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240123/십진수와 이진수/decimal-and-binary-number.swift
+++ 240123/십진수와 이진수/decimal-and-binary-number.swift
@@ -1,9 +1,9 @@
 import Foundation
-// let input = Int(readLine()!)! * 17
-// let ans = String(input, radix : 2)
-// print(ans)
+
 
 let input = readLine()!
-let trans = Int(input, radix: 2)! * 17
-let ans = String(trans, radix: 2)
-print(ans)
+if let trans = Int(input, radix: 2) {
+    let ans = String(trans * 17 , radix: 2)
+    print(ans)
+}
+// let ans = String(trans, radix: 2)
