a20b6998bd73cfabb81910653330886fa72676aa
Yimkeul <leesjun29@gmail.com>
2024-01-23 22:08:58 +0900

    Title: 십진수와 이진수, Time: 11ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240123/십진수와 이진수/decimal-and-binary-number.swift
+++ 240123/십진수와 이진수/decimal-and-binary-number.swift
@@ -5,5 +5,7 @@ let input = readLine()!
 if let trans = Int(input, radix: 2) {
     let ans = String(trans * 17 , radix: 2)
     print(ans)
+} else {
+    print("자리수 초과")
 }
 // let ans = String(trans, radix: 2)
