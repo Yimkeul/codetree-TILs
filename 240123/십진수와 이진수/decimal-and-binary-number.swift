3bf2cc2e40bad3ec2c426555edb9d6ea8369a78e
Yimkeul <leesjun29@gmail.com>
2024-01-23 22:04:10 +0900

    Title: 십진수와 이진수, Time: 20ms, Memory: 1MB, Status: Runtime Error - CodeTree

--- 240123/십진수와 이진수/decimal-and-binary-number.swift
+++ 240123/십진수와 이진수/decimal-and-binary-number.swift
@@ -1,1 +1,9 @@
-print(String(Int(readLine()!, radix: 2)! * 17, radix: 2))
+import Foundation
+// let input = Int(readLine()!)! * 17
+// let ans = String(input, radix : 2)
+// print(ans)
+
+let input = readLine()!
+let trans = Int(input, radix: 2)! * 17
+let ans = String(trans, radix: 2)
+print(ans)
