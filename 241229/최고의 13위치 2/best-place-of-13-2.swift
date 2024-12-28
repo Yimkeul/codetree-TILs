743a0028e13c97691f441fa7d1255fdc10cd0ebd
SeJun Lee <leesjun29@gmail.com>
2024-12-29 02:51:06 +0900

    Title: 최고의 13위치 2, Time: 10ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 241229/최고의 13위치 2/best-place-of-13-2.swift
@@ -0,0 +1,61 @@
+let N = Int(readLine()!)!
+
+var map = [[Int]]()
+
+for _ in 0 ..< N {
+    let input = readLine()!.split { $0 == " "}.map { Int($0)! }
+    map.append(input)
+}
+
+var ans = 0
+
+var bar1 = 0
+var check:(Int,Int) = (0,0)
+var bar2 = 0
+
+var dxs: [Int] = [0,1,2]
+
+for i in 0 ..< N {
+    for j in 0 ..< N {
+        var curx = j
+        var temp = 0
+        for dx in dxs {
+            var nx = curx + dx
+            if !isRange(i,nx) {
+                break
+            }
+            if map[i][nx] == 1 {
+                temp += 1
+            }
+        }
+        if bar1 < temp {
+            bar1 = temp
+            check = (i,j)
+        }
+    }
+}
+
+for i in 0 ..< N {
+    for j in 0 ..< N {
+        var curx = j
+        var temp = 0
+        for dx in dxs {
+            var nx = curx + dx
+            if !isRange(i,nx) || nx == check.1 {
+                break
+            }
+            if map[i][nx] == 1 {
+                temp += 1
+            }
+        }
+        if bar2 < temp {
+            bar2 = temp
+        }
+    }
+}
+print(bar1 + bar2)
+
+func isRange(_ x: Int, _ y: Int) -> Bool {
+    return 0 <= x && x < N && 0 <= y && y < N
+}
+
