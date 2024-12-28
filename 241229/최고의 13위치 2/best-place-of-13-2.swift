ba01a37c4cc297ae1192213995d30f4fca2a2452
SeJun Lee <leesjun29@gmail.com>
2024-12-29 03:09:32 +0900

    Title: 최고의 13위치 2, Time: 12ms, Memory: 1MB, Status: Passed - CodeTree

--- 241229/최고의 13위치 2/best-place-of-13-2.swift
+++ 241229/최고의 13위치 2/best-place-of-13-2.swift
@@ -1,70 +1,30 @@
-let N = Int(readLine()!)!
+import Foundation
 
-var map = [[Int]]()
-
-for _ in 0 ..< N {
-    let input = readLine()!.split { $0 == " "}.map { Int($0)! }
-    map.append(input)
+// 입력 처리
+let n = Int(readLine()!)!
+var arr = [[Int]]()
+for _ in 0..<n {
+    let row = readLine()!.split(separator: " ").map { Int($0)! }
+    arr.append(row)
 }
 
+// 알고리즘 구현
+var maxCnt = 0
 
-var bar1 = 0
-var check1:(Int,Int) = (0,0)
-var check2:(Int,Int) = (0,0)
-var bar2 = 0
-
-var dxs: [Int] = [0,1,2]
-
-for i in 0 ..< N {
-    for j in 0 ..< N {
-        var curx = j
-        var temp = 0
-        for dx in dxs {
-            var nx = curx + dx
-            if !isRange(i,nx) {
-                break
-            }
-            if map[i][nx] == 1 {
-                temp += 1
-            }
-            if dx == 2 {
-                if bar1 < temp {
-                    bar1 = temp
-                    check1 = (i,j)
-                    check2 = (i,j + 2)
-                    // print("bar1",bar1,check1,check2)
+for i in 0..<n {
+    for j in 0..<(n - 2) {
+        for k in 0..<n {
+            for l in 0..<(n - 2) {
+                if i == k && abs(j - l) <= 2 {
+                    continue
                 }
+                
+                let cnt1 = arr[i][j] + arr[i][j + 1] + arr[i][j + 2]
+                let cnt2 = arr[k][l] + arr[k][l + 1] + arr[k][l + 2]
+                maxCnt = max(maxCnt, cnt1 + cnt2)
             }
         }
-        
     }
 }
 
-for i in 0 ..< N {
-    for j in 0 ..< N {
-        var curx = j
-        var temp = 0
-        for dx in dxs {
-            var nx = curx + dx
-            if !isRange(i,nx) || (i,nx) == check1 || (i,nx) == check2 {
-                break
-            }
-            if map[i][nx] == 1 {
-                temp += 1
-            }
-            if dx == 2 {
-                if bar2 < temp {
-                    bar2 = temp
-                    // print("bar2",bar2,i,j)
-                }
-            }
-        }
-        
-    }
-}
-print(bar1 + bar2)
-
-func isRange(_ x: Int, _ y: Int) -> Bool {
-    return 0 <= x && x < N && 0 <= y && y < N
-}
-
+print(maxCnt)
