94a8415577d9955cb39a601841789ce7c2bfd419
SeJun Lee <leesjun29@gmail.com>
2025-01-01 19:52:01 +0900

    Title: 아름다운 수열 2, Time: 9ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 250101/아름다운 수열 2/beautiful-sequence-2.swift
+++ 250101/아름다운 수열 2/beautiful-sequence-2.swift
@@ -2,31 +2,32 @@ let NM = readLine()!.split { $0 == " "}.map { Int($0)! }
 var arrayA = readLine()!.split { $0 == " "}.map { Int($0)! }
 var arrayB = readLine()!.split { $0 == " "}.map { Int($0)! }
 
-var A = NM[0]
-var B = NM[1]
-if NM[0] < NM[1] {
-    var temp = arrayB
-    arrayB = arrayA
-    arrayA = temp
-    A = NM[1]
-    B = NM[0]
-}
-
-
-
 var cnt = 0
-for i in 0 ... A - B {
-    var isTrue = false
-    for j in i ..< i + B {
-        if arrayB.contains(arrayA[j]) {
-            isTrue = true
-        } else { 
-            isTrue = false
-            break
+if NM[0] < NM[1] {
+    // var temp = arrayB
+    // arrayB = arrayA
+    // arrayA = temp
+    // A = NM[1]
+    // B = NM[0]
+    print(cnt)
+    
+} else {
+    for i in 0 ... NM[0] - NM[1] {
+        var isTrue = false
+        for j in i ..< i + NM[1] {
+            if arrayB.contains(arrayA[j]) {
+                isTrue = true
+            } else { 
+             isTrue = false
+                break
+            }
+        }
+        if isTrue {
+            cnt += 1
         }
     }
-    if isTrue {
-        cnt += 1
-    }
+    print(cnt)
 }
-print(cnt)
+
+
+
